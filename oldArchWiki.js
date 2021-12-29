69951324f48409fdc5b0e6982168f2647e701076
Benjamin Herne <2001herne@gmail.com>
2021-12-29 18:16:24 +1100

    Added configuration options

--- oldArchWiki.js
+++ oldArchWiki.js
@@ -1,5 +1,30 @@
-let currentUrl = new URL(window.location.href);
-if (!currentUrl.searchParams.has("useskinversion")) {
-    currentUrl.searchParams.set("useskinversion", "1")
-    window.location.href = currentUrl.toString()
+function onGet(data) {
+    const minWidth = data.minWidth || defaultMinWidth;
+    const maxWidth = data.maxWidth || defaultMaxWidth;
+    const minCheck = data.minCheck || defaultCheckState;
+    const maxCheck = data.maxCheck || defaultCheckState;
+
+    const currWidth = document.documentElement.clientWidth;
+
+    let skinVersion = 1;
+    if ((minCheck && currWidth <= minWidth) || (maxCheck && currWidth >= maxWidth)) {
+        skinVersion = 2;
+    }
+
+    let currentUrl = new URL(window.location.href);
+    const currentSkinVersion = currentUrl.searchParams.get("useskinversion");
+    if (skinVersion !== parseInt(currentSkinVersion)) {
+        currentUrl.searchParams.set("useskinversion", String(skinVersion))
+        window.location.href = currentUrl.toString()
+    }
+
+    console.log()
+
 }
+
+let defaultMinWidth = 960;
+let defaultMaxWidth = 3840;
+let defaultCheckState = false;
+
+const data = browser.storage.sync.get(["minWidth", "minCheck", "maxWidth", "maxCheck"]);
+data.then(onGet)
